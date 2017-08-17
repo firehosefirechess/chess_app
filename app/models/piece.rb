c8d581beee2cf27d5c1d7ba6ca01fba4fe1eff53
ender554 <joshadams554@gmail.com>
2017-08-17 13:24:09 -0700

    added general rules for all pieces

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,27 @@
-class Piece < ApplicationRecord
+class Piece < ActiveRecord::Base
+  belongs_to :player
+  belongs_to :game
+
+  def color
+  end
+
+  def move(row,col)
+  end
+
+  def is_obstructed?(dest_row, dest_col)
+  end
+
+  def horizontal_move(x,y)
+  end
+
+  def vertical_move(x,y)
+  end
+
+  def diagonal_move(x,y)
+  end
+
+  def capture(x,y)
+  end
+
+
 end
