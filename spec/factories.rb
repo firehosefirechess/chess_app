557a738b02b21cb789fa841e6c23c3f4130fa579
Christian Van <khanhvan121992@gmail.com>
2017-08-20 21:50:26 +0000

    minor edit for model rspec files

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,24 +1,32 @@
 FactoryGirl.define do
   factory :king do
-    
+
   end
+
   factory :queen do
-    
+
   end
+
   factory :bishop do
-    
+
   end
+
   factory :knight do
-    
+
   end
+
   factory :rook do
+
+  end
+
+  factory :pawn do
     
   end
-  factory :pawn do
-  end
+
   factory :piece do
     type ""
   end
+
   factory :user do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,18 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
- 
 
   describe "Pawn type" do
     it "moves up 1 or 2 squares" do
-      
+
     end
 
     it "captures diagonally" do
-      
+
     end
   end
 
+  describe "Bishop type" do
+    it "should move vertically or horizontally" do
+
+    end
+
+    it "should captures units vertically or horizontally" do
+
+    end
+  end
+
+  describe "King type" do
+    it "should move up, down, diagonal 1 squares" do
+
+    end
+
+    it "should captures units vertically, horizontally, and diagonally" do
+
+    end
+  end
+
+  describe "Knight type" do
+    it "should move " do
+
+    end
+
+    it "should capture units " do
+
+    end
+  end
+
+  describe "Queen type" do
+    it "should move vertically, horizontally, and diagonally" do
+
+    end
+
+    it "should captures units vertically, horizontally, and diagonally" do
+
+    end
+  end
+
+  describe "Rook type" do
+    it "should move diagonally" do
+
+    end
+
+    it "should captures units diagonally" do
+      
+    end
 end
-
-
