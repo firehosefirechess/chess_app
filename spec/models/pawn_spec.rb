e76703e1683532e70ad348a25b9d814486c749ab
asyaasha <asya.asha@gmail.com>
2017-08-20 21:14:04 -0400

    add test for the rook

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Knight, type: :model do
+RSpec.describe Pawn, type: :model do
   let(:pawn) { FactoryGirl.build :pawn }
   it 'is a Piece type' do
     expect(pawn).to be_a_kind_of(Piece)
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
-  it 'should move diagonally' do
+  let(:rook) { FactoryGirl.build :rook }
+  it 'is a Piece type' do
+    expect(rook).to be_a_kind_of(Piece)
   end
+  # it 'should move diagonally' do
+  # end
 
-  it 'should captures units diagonally' do
-  end
+  # it 'should captures units diagonally' do
+  # end
 end
