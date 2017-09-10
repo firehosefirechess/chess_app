e558c5f7594ce6bd133c69b376133e4611b222b6
Christian Van <khanhvan121992@gmail.com>
2017-09-10 10:02:54 +0000

    Add diagonal_obstruction test

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,5 +1,6 @@
 class Piece < ApplicationRecord
   # belongs_to :player # no player model yet
+  # rubocop:disable Metrics/AbcSize
   belongs_to :game
   validates :color, presence: true
   validates :type, presence: true
@@ -33,17 +34,27 @@ class Piece < ApplicationRecord
     range = self.y_position < destination_y ? (self.y_position..destination_y) : (destination_y..self.y_position)
     game.pieces.where(
         color: self.color,
-        x_position: self.x_position, 
+        x_position: self.x_position,
         y_position: range
       ).where.not(id: self.id).any?
   end
-  
+
   def horizontal_obstruction?(destination_x)
     range = self.x_position < destination_x ? (self.x_position..destination_x) : (destination_x..self.x_position)
     game.pieces.where(
         color: self.color,
-        x_position: range, 
+        x_position: range,
         y_position: self.y_position
       ).where.not(id: self.id).any?
+  end
+
+  def diagonal_obstruction?(destination_x, destination_y)
+    x_range = self.x_position < destination_x ? (self.x_position..destination_x) : (destination_x..self.x_position)
+    y_range = self.y_position < destination_y ? (self.y_position..destination_y) : (destination_y..self.y_position)
+    game.pieces.where(
+      color: self.color,
+      x_position: x_range,
+      y_position: y_range
+    ).where.not(id: self.id).any?
   end
 end
